8e42b7d18756e0316f9cbbf147f608ac116eba23
Kazuto Kotera <st002009@m01.kyoto-kcg.ac.jp>
2025-11-27 15:39:37 +0900

    Revert "add item view"
    This reverts commit e47995200719d5738e8af2120a8a2b88446f2d21.

--- WpfApp1/GalleryView.xaml.cs
+++ WpfApp1/GalleryView.xaml.cs
@@ -1,63 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 
 namespace WpfApp1
 {
+
     public partial class GalleryView : UserControl
     {
+        // 戻るボタンが押されたら通知するためのイベント
         public event Action BackRequested;
-
-        private List<string> items; // データアイテム（例として文字列）
-        private int currentPage = 0;
-        private int itemsPerPage = 15; // 3行×5列
 
         public GalleryView()
         {
             InitializeComponent();
-            LoadItems();
-            DisplayPage();
-        }
-
-        private void LoadItems()
-        {
-            // サンプルデータ 1～300 を作成
-            items = new List<string>();
-            for (int i = 1; i <= 300; i++)
-                items.Add($"Item {i}");
-        }
-
-        private void DisplayPage()
-        {
-            ItemGrid.Items.Clear();
-            int start = currentPage * itemsPerPage;
-            int end = Math.Min(start + itemsPerPage, items.Count);
-
-            for (int i = start; i < end; i++)
-            {
-                var btn = new Button { Content = items[i], Margin = new Thickness(5) };
-                btn.Click += (s, e) => MessageBox.Show($"選択: {items[i]}");
-                ItemGrid.Items.Add(btn);
-            }
-        }
-
-        private void PrevPage_Click(object sender, RoutedEventArgs e)
-        {
-            if (currentPage > 0)
-            {
-                currentPage--;
-                DisplayPage();
-            }
-        }
-
-        private void NextPage_Click(object sender, RoutedEventArgs e)
-        {
-            if ((currentPage + 1) * itemsPerPage < items.Count)
-            {
-                currentPage++;
-                DisplayPage();
-            }
         }
 
         private void Back_Click(object sender, RoutedEventArgs e)
